1dadd13362b59602376948e6eedc0844dc6df8e5
Mauaqp <peraltilla93@hotmail.com>
2022-02-01 13:49:44 -0500

    Entrega final

--- flask_app/controllers/c_users.py
+++ flask_app/controllers/c_users.py
@@ -4,20 +4,57 @@ from flask_app.models.m_user import User
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt( app )
+
 #rutas principales
 @app.route( '/', methods=['GET'] )
 def inicio():
     return render_template( "index.html" )
 
-@app.route('/dashboard', methods=['GET'])
-def dashboard():
-    return render_template("dashboard.html")
-
-#Validación con re
+#REGISTRAR USUARIOS
 @app.route('/register', methods=['POST'])
 def register():
-    if not User.validate_user(request.form):
-        # redirigimos a la plantilla con el formulario
+    #validación
+    if not User.validate_register(request.form):
         return redirect('/')
-    # ...hacer otras cosas
+
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": bcrypt.generate_password_hash(request.form['password'])
+    }
+    #Datos de session
+    id = User.addUser(data)
+    session['user_id'] = id
+
     return redirect('/dashboard')
+
+#RUTA LOGIN
+@app.route('/login',methods=['POST'])
+def login():
+    user = User.get_by_email(request.form)
+
+    if not user:
+        flash("Invalid Email","login")
+        return redirect('/')
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash("Invalid Password","login")
+        return redirect('/')
+    session['user_id'] = user.id
+    return redirect('/dashboard')
+
+#Ruta dashboard
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("dashboard.html",user=User.get_by_id(data))
+
+#Logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
